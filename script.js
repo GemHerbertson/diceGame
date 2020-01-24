6115702ce61c6ed15f1d091fd9138b67ada828e0
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-01-24 17:25:45 +0000

    updated CSS

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
 // HTML declarations 
 
-
-// const playerId = document.getElementById("playerId")
 const playerScore = document.getElementById("playerScore");
 const totalScore = document.getElementById("totalScore");
 const diceImage = document.getElementById("diceImage");
 const scoreMessage = document.getElementById("scoreMessage");
 const rollButton = document.getElementById("roll");
 
-
-// const resetButton = document.getElementById("reset");
-
 diceImage.style.visibility = "hidden";
+
+// JS Variables
 
 let numberRolled;
 let pointScored = 0;
-// let playerTurn = true;
 
 // Function declarations
 
 const diceRoll = () => {
     numberRolled=(Math.ceil(Math.random() * 6));
-    // console.log(`You've rolled ${numberRolled}`);
 }
 
 const cummulativeScore = () => {
     diceRoll()
     pointScored += numberRolled;
-    // console.log(`Your score is ${pointScored}`);
 }
 
 const winOrLose = () => {
-    // cummulativeScore()
     if (numberRolled == 1) {
         scoreMessage.textContent = "You Lose! Next player step up...";
         rollButton.textContent = "Play again";
         pointScored = 0;
-    } else if (pointScored >= 20) {
+    } else if (pointScored >= 21) {
         scoreMessage.textContent = "You Win!";
         rollButton.textContent = "Play again";
         pointScored = 0;
@@ -46,24 +39,11 @@ const winOrLose = () => {
     }
 }
 
-// const activeplayer = () => {
-//     if (playerTurn) {
-//         playerId.textContent = "Player One"
-//         playerTurn = false
-//     } else 
-//         playerId.textContent = "Player Two"
-//         playerOneTurn = true
-// }
-
 rollButton.addEventListener("click", () => {
     diceRoll()
-    playerScore.textContent = `You have rolled ${numberRolled}`
-    totalScore.textContent = `Your score is ${pointScored += numberRolled}`
+    playerScore.textContent = `You rolled ${numberRolled}`
+    totalScore.textContent = `Your total score is ${pointScored += numberRolled}`
     diceImage.style.visibility = "visible";
     diceImage.src = `img/dice${numberRolled}.png`;
     winOrLose()
 })
-
-// diceRoll()
-// cummulativeScore()
-// winOrLose()
